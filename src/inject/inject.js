ca8b817f9e0dfab5485d7750307a074adfee0ebb
Nick Quinlan <nick@nicholasquinlan.com>
2021-07-21 11:11:41 -0700

    updated to naively handle slow loading

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,17 @@
-window.addEventListener('popstate', function (event) {
-	const header = ["h1", "h2", "h3", "h4"].map(candidate => {
-		return document.querySelectorAll(candidate)[0] || document.querySelectorAll("." + candidate)[0];
-	}).filter(x => x != null)[0]?.innerText;
-	const section = document.querySelectorAll(".main-menu .current")[0]?.innerText;
-	document.title = [header, section, "Tipalti"].filter(x => x != undefined).join(" - ");
-});
+(function () {
+	const updateTitle = function (event) {
+		const header = ["h1", "h2", "h3", "h4"].map(candidate => {
+			return document.querySelectorAll(candidate)[0] || document.querySelectorAll("." + candidate)[0];
+		}).filter(x => x != null)[0]?.innerText;
+		const section = document.querySelectorAll(".main-menu .current")[0]?.innerText;
+		document.title = [header, section, "Tipalti"].filter(x => x != undefined).join(" - ");
+	}
+
+	const waitAndUpdateTitle = function (time) {
+		setTimeout(updateTitle, time || 500);
+	}
+
+	window.addEventListener('popstate', waitAndUpdateTitle);
+	waitAndUpdateTitle(1000);
+	
+})()
