3795715c2cf8272fee57ba9c0a60c572a7740555
Nick Quinlan <nick@nicholasquinlan.com>
2021-07-21 11:14:21 -0700

    included subtabs

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,8 +3,9 @@
 		const header = ["h1", "h2", "h3", "h4"].map(candidate => {
 			return document.querySelectorAll(candidate)[0] || document.querySelectorAll("." + candidate)[0];
 		}).filter(x => x != null)[0]?.innerText;
+		const tab = document.querySelectorAll(".nav-tabs .active")[0].innerText;
 		const section = document.querySelectorAll(".main-menu .current")[0]?.innerText;
-		document.title = [header, section, "Tipalti"].filter(x => x != undefined).join(" - ");
+		document.title = [header, tab, section, "Tipalti"].filter(x => x != undefined).join(" - ");
 	}
 
 	const waitAndUpdateTitle = function (time) {
